e49e89f4b124c6c349fd5dc53147dd38da0c2b09
Biraj Zalavadia <biraj@tasolglobal.com>
2017-02-08 10:45:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/tasol/scanthat/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.tasol.scanthat;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    //View Objects
+    private Button buttonScan;
+    private TextView textViewName, textViewAddress;
+
+    //qr code scanner object
+    private IntentIntegrator qrScan;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //View objects
+        buttonScan = (Button) findViewById(R.id.buttonScan);
+        textViewName = (TextView) findViewById(R.id.textViewName);
+        //intializing scan object
+        qrScan = new IntentIntegrator(this);
+
+        //attaching onclick listener
+        buttonScan.setOnClickListener(this);
+    }
+
+    //Getting the scan results
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            //if qrcode has nothing in it
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Result Not Found", Toast.LENGTH_LONG).show();
+            } else {
+                //if qr contains data
+                    //converting the data to json
+//                    JSONObject obj = new JSONObject(result.getContents());
+                    //setting values to textviews
+                if(result.getContents().equals("BINGO")){
+                    textViewName.setText("Payment Successful");
+                }else{
+                    textViewName.setText("Payment Failed");
+                }
+
+//                    textViewAddress.setText(obj.getString("address"));
+
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        //initiating the qr code scan
+        qrScan.initiateScan();
+    }
+}
